abb0c55f515f3c89dcb88fc0f2a6995910ccabcd
moskvichev evgeniy <moskvichev_e@bk.ru>
2024-08-02 14:32:59 +0400

    script

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let cite = document.querySelectorAll('.cite');
+// cite[1].style.textAlign = 'right';
+
+// for (let i = 0; i == cite[1]; i++) {
+
+//     cite[1].style.textAlign = 'right';
+
+// }
